3712f49eeaac2cff0b04b383c95bf2cf9f65b1ec
Alastair Houghton <ahoughton@apple.com>
2023-08-11 16:37:53 +0100

    [WIP] Support for building static binaries with Musl and Swift.
    This is very much a work in progress.

--- clang/lib/Headers/stddef.h
+++ clang/lib/Headers/stddef.h
@@ -7,6 +7,13 @@
  *===-----------------------------------------------------------------------===
  */
 
+#if defined(__musl__)
+
+// On musl systems, use the system header
+#include_next <stddef.h>
+
+#else
+
 #if !defined(__STDDEF_H) || defined(__need_ptrdiff_t) ||                       \
     defined(__need_size_t) || defined(__need_rsize_t) ||                       \
     defined(__need_wchar_t) || defined(__need_NULL) ||                         \
@@ -105,3 +112,5 @@ __WINT_TYPE__ directly; accommodate both by requiring __need_wint_t */
 #endif /* __need_wint_t */
 
 #endif
+
+#endif /* !defined(__musl__) */
